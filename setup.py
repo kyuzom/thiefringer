812108efab947a1af3b5503c397cbdea4f421bc3
Zoltan <zotheconqueror@googlegroups.com>
2021-12-03 14:27:09 +0100

    Integrate GSM support (SMS message sending)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="thiefringer",
-    version="0.0.1",
+    version="0.0.2",
     author="KyuzoM",
     author_email="99549950+kyuzom@users.noreply.github.com",
     description="ThiefRinger alarm system",
@@ -16,7 +16,9 @@ setuptools.setup(
         "thiefringer": ["**/*"],
     },
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
